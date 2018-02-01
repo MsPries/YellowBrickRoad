b7ddf1a96244d03a3da133a41ba145b1566cef95
Ms Pries <emily_pries@milton.edu>
2018-02-01 07:48:50 -0500

    Adds the game

---
+++ World.py
@@ -0,0 +1,155 @@
+'''
+Modified for Python 3 from Siraj Raval's https://github.com/llSourcell/q_learning_demo
+'''
+
+__author__ = 'philippe'
+from tkinter import *
+master = Tk()
+
+triangle_size = 0.1
+cell_score_min = -0.2
+cell_score_max = 0.2
+Width = 25
+(x, y) = (17, 10)
+actions = ["down", "left", "up", "right"]
+wins = 0
+
+board = Canvas(master, width=x*Width, height=y*Width)
+player = (0, y-1)
+score = 1
+restart = False
+walk_reward = -0.04
+
+walls = [(1, 1), (2, 1), (3, 1), (8, 1), (9, 1), (10, 1), (12, 1), (14, 1),
+(1, 2), (3, 2), (8, 2), (10, 2), (12, 2), (14, 2),
+(1, 3), (2, 3), (3, 3), (8, 3), (9,3), (10, 3), (12, 3), (13, 3), (14, 3),
+(1, 4), (10, 4), (14, 4), (1,5), (3, 5), (4, 5), (8,5), (9,5), (10,5), (14, 5), (3,6), (14, 6),
+(3, 7), (5,7), (6,7), (7,7), (3,8), (5,8), (7,8), (5,9), (6,9), (7,9)] 
+specials = [(16, 3, "yellow", 1)]
+cell_scores = {}
+
+
+def create_triangle(i, j, action):
+    if action == actions[0]:
+        return board.create_polygon((i+0.5-triangle_size)*Width, (j+triangle_size)*Width,
+                                    (i+0.5+triangle_size)*Width, (j+triangle_size)*Width,
+                                    (i+0.5)*Width, j*Width,
+                                    fill="white", width=1)
+    elif action == actions[1]:
+        return board.create_polygon((i+0.5-triangle_size)*Width, (j+1-triangle_size)*Width,
+                                    (i+0.5+triangle_size)*Width, (j+1-triangle_size)*Width,
+                                    (i+0.5)*Width, (j+1)*Width,
+                                    fill="white", width=1)
+    elif action == actions[2]:
+        return board.create_polygon((i+triangle_size)*Width, (j+0.5-triangle_size)*Width,
+                                    (i+triangle_size)*Width, (j+0.5+triangle_size)*Width,
+                                    i*Width, (j+0.5)*Width,
+                                    fill="white", width=1)
+    elif action == actions[3]:
+        return board.create_polygon((i+1-triangle_size)*Width, (j+0.5-triangle_size)*Width,
+                                    (i+1-triangle_size)*Width, (j+0.5+triangle_size)*Width,
+                                    (i+1)*Width, (j+0.5)*Width,
+                                    fill="white", width=1)
+
+
+def render_grid():
+    global specials, walls, Width, x, y, player
+    for i in range(x):
+        for j in range(y):
+            board.create_rectangle(i*Width, j*Width, (i+1)*Width, (j+1)*Width, fill="white", width=1)
+            temp = {}
+            for action in actions:
+                temp[action] = create_triangle(i, j, action)
+            cell_scores[(i,j)] = temp
+    for (i, j, c, w) in specials:
+        board.create_rectangle(i*Width, j*Width, (i+1)*Width, (j+1)*Width, fill=c, width=1)
+    for (i, j) in walls:
+        board.create_rectangle(i*Width, j*Width, (i+1)*Width, (j+1)*Width, fill="black", width=1)
+
+render_grid()
+
+
+def set_cell_score(state, action, val):
+    global cell_score_min, cell_score_max
+    triangle = cell_scores[state][action]
+    green_dec = int(min(255, max(0, (val - cell_score_min) * 255.0 / (cell_score_max - cell_score_min))))
+    green = hex(green_dec)[2:]
+    red = hex(255-green_dec)[2:]
+    if len(red) == 1:
+        red += "0"
+    if len(green) == 1:
+        green += "0"
+    color = "#" + red + green + "00"
+    board.itemconfigure(triangle, fill=color)
+
+
+def try_move(dx, dy):
+    global player, x, y, score, walk_reward, me, restart, wins
+    if restart == True:
+        restart_game()
+    new_x = player[0] + dx
+    new_y = player[1] + dy
+    score += walk_reward
+    if (new_x >= 0) and (new_x < x) and (new_y >= 0) and (new_y < y) and not ((new_x, new_y) in walls):
+        board.coords(me, new_x*Width+Width*2/10, new_y*Width+Width*2/10, new_x*Width+Width*8/10, new_y*Width+Width*8/10)
+        player = (new_x, new_y)
+    for (i, j, c, w) in specials:
+        if new_x == i and new_y == j:
+            score -= walk_reward
+            score += w
+            if score > 0:
+                print("Score: ", score)
+                wins += 1
+                print("You earned brick", wins, "out of 5")
+                if wins == 2:
+                    print ("Winner! Here's your clue! Left", (2*(int(chr(51))**int(chr(50))-int(chr(53)))))
+                    raise SystemExit
+            else:
+                print("Score: ", score)
+                if wins > 0:
+                    print("Oh no! The wicked witch stole your bricks :(")
+                    wins = 0
+            restart = True
+            return
+
+
+def call_up(event):
+    try_move(0, -1)
+
+
+def call_down(event):
+    try_move(0, 1)
+
+
+def call_left(event):
+    try_move(-1, 0)
+
+
+def call_right(event):
+    try_move(1, 0)
+
+
+def restart_game():
+    global player, score, me, restart
+    player = (0, y-1)
+    score = 1
+    restart = False
+    board.coords(me, player[0]*Width+Width*2/10, player[1]*Width+Width*2/10, player[0]*Width+Width*8/10, player[1]*Width+Width*8/10)
+
+def has_restarted():
+    return restart
+
+master.bind("<Up>", call_up)
+master.bind("<Down>", call_down)
+master.bind("<Right>", call_right)
+master.bind("<Left>", call_left)
+
+me = board.create_rectangle(player[0]*Width+Width*2/10, player[1]*Width+Width*2/10,
+                            player[0]*Width+Width*8/10, player[1]*Width+Width*8/10, fill="orange", width=1, tag="me")
+
+board.grid(row=0, column=0)
+
+
+def start_game():
+    print("Collect 5 yellow bricks to build your own brick road.\n\nYou get a brick each time you navigate to the yellow square and arrive with a postive score.\n\nIf you score a negative score, you'll lose any bricks you previously earned.\n\nGood luck!!\n\n\n")
+    master.mainloop()
